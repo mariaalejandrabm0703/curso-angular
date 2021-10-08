d01525390d2ed1e775a6250ab76ab36fbf5b0504
Maria Alejandra Bohorquez Martheyn - Ceiba Software <maria.bohorquez@ceiba.com.co>
2021-10-07 22:16:01 -0500

    se modifica componente home para obtener un excel y leerlo. Devuelve un jso con la info.

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-
+  title= 'Upload files'
+  convertedJson = ''
   constructor() { }
 
   ngOnInit() {
   }
 
+  fileUpload(event: Event){
+    const element = event.target as HTMLInputElement;
+    let files = element.files;
+    if (files) {
+      console.log("FileUpload -> files", files);
+      const fileReader = new FileReader();
+      fileReader.readAsBinaryString(files[0])
+      fileReader.onload = (e) => {
+        let binaryData = e.target.result;
+        let workbook = XLSX.read(binaryData, {type: 'binary'});
+        workbook.SheetNames.forEach(sheet =>{
+          const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+          this.convertedJson = JSON.stringify(data, undefined, 4);
+        });
+      }
+    }
+  }
 }
