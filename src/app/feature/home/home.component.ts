681d18afaa100377bc8ce53b32c5a4ee0074535c
Maria Alejandra Bohorquez Martheyn - Ceiba Software <maria.bohorquez@ceiba.com.co>
2021-10-08 08:26:35 -0500

    se modifica para guardar varias hojas de un excel.

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,34 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import * as XLSX from 'xlsx';
+import { Component, OnInit } from "@angular/core";
+import * as XLSX from "xlsx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  title= 'Upload files'
-  convertedJson = ''
-  constructor() { }
+  title = "Upload files";
+  convertedJson = "";
+  totalJson = { sheets: new Array()};
 
-  ngOnInit() {
-  }
+  constructor() {}
 
-  fileUpload(event: Event){
+  ngOnInit() {}
+
+ fileUpload(event: Event) {
+  //  debugger;
     const element = event.target as HTMLInputElement;
     let files = element.files;
     if (files) {
-      console.log("FileUpload -> files", files);
       const fileReader = new FileReader();
-      fileReader.readAsBinaryString(files[0])
-      fileReader.onload = (e) => {
-        let binaryData = e.target.result;
-        let workbook = XLSX.read(binaryData, {type: 'binary'});
-        workbook.SheetNames.forEach(sheet =>{
-          const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
-          this.convertedJson = JSON.stringify(data, undefined, 4);
-        });
-      }
+      fileReader.readAsBinaryString(files[0]);
+
+    fileReader.onload = async (e)  => {
+      let binaryData = e.target.result;
+      let workbook =  await XLSX.read(binaryData, { type: "binary" });
+      workbook.SheetNames.forEach((sheet) => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+        this.totalJson.sheets.push(data)
+        // this.convertedJson = JSON.stringify(data, undefined, 4);
+      });
+        console.log(this.totalJson)
+      };
     }
   }
 }
